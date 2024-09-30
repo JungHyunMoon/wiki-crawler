33ae039f5e39649376b5971fa321b881b9ab3a5c
junghyunmoon <foqlzm12345@gmail.com>
2024-09-30 17:48:25 +0900

    feat : scheduler
    크롤러 스케줄링 구현
    vector_store를 chroma로 변경
    이에 따른 streamlit cloud 쪽으로의 업데이트는 일시적 중단

--- VecDBLoader.py
+++ VecDBLoader.py
@@ -63,6 +63,40 @@ def embedding_text_line(source, text_list, document_version):
     time.sleep(1)
 
 
+def embedding_text_line_pinecone(source, text_list):
+    # text_list가 리스트인지 확인
+    if not isinstance(text_list, list):
+        raise ValueError("text_list는 리스트여야 합니다.")
+
+    # 리스트의 각 항목이 문자열인지 확인
+    for text in text_list:
+        if not isinstance(text, str):
+            raise ValueError("리스트의 각 항목은 문자열이어야 합니다.")
+
+    # 리스트를 하나의 문자열로 결합
+    combined_text = " ".join(text_list)
+
+    # Document 객체 생성 및 source 정보 추가
+    document = Document(page_content=combined_text, metadata={"source": source} if source else {})
+
+    index_name = "wiki-upstage-index"
+    chunk = 500
+    namespace = f"chunk_{chunk}_v1"
+
+    # 텍스트 데이터를 분할하여 처리
+    document_list = load_and_split_from_text([document], chunk)
+    embeddings = get_embeddings()
+
+    PineconeVectorStore.from_documents(
+        index_name=index_name,
+        namespace=namespace,
+        documents=document_list,
+        embedding=embeddings
+    )
+    time.sleep(1)
+
+
+
 def load_and_split_from_text(documents, chunk):
     """텍스트 데이터를 직접 처리하는 메서드"""
     splitter = RecursiveCharacterTextSplitter(
