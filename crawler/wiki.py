835cfed76ace2140b7ddb02cde74b235b447cd83
junghyunmoon <foqlzm12345@gmail.com>
2024-10-10 17:12:35 +0900

    feat : logger
    문서 갱신 작업시 로그파일 생성 기능 추가

--- crawler/wiki.py
+++ crawler/wiki.py
@@ -10,7 +10,6 @@ from dotenv import load_dotenv
 from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup, Tag
-from nltk.corpus.reader import documents
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -22,7 +21,7 @@ def login(driver, url, user_id, user_pw):
     driver.get(url)
 
     # 페이지 로드 대기
-    time.sleep(3)  # JavaScript가 실행될 시간을 기다림
+    time.sleep(5)  # JavaScript가 실행될 시간을 기다림
 
     # ID, PW 입력
     driver.find_element(By.ID, 'input-20').send_keys(user_id)
